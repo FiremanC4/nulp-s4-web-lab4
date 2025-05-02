577bf16cb6d9fa08d8287de78ddfbc4f1c8599b4
Mykhailo <firemanplay66@gmail.com>
2025-05-02 12:23:55 +0300

    this woul maybe fix 404 refresh error

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        injectData: {
+          title: 'My App',
+        },
+      },
+    }),
+  ],
   base: '/nulp-s4-web-lab4/',
-})
+  build: {
+    outDir: 'nulp-s4-web-lab4',
+    publicPath: '/',
+    emptyOutDir: true, // also necessary
+  },
+  server: {
+    historyApiFallback: true, // Ensures SPA fallback
+  },
+});
