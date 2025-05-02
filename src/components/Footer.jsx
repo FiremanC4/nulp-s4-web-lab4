cb706112d4ef033fad835541464cc62a2d11b4a2
Mykhailo <firemanplay66@gmail.com>
2025-05-02 16:13:46 +0300

    renamed Author

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
       <p className="copyright">
         &copy; 2025 TourBlog. <br /> Всі права захищені.
       </p>
-      <p className="author-name">Прізвище Ім'я</p>
+      <p className="author-name">FiremanC4</p>
       <a href="https://github.com/FiremanC4" className="github-link">
         <img
           src="/nulp-s4-web-lab4/img/icons/github-mark/github-mark.png"
